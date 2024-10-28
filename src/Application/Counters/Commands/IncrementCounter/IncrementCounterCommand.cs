4eabd07c274d2bdb0289cec9cab9a928adbaaea8
Timofey Votyakov <tvotyakov@gmail.com>
2024-10-28 15:56:11 +0100

    Validate if counter increment value is positive

--- src/Application/Counters/Commands/IncrementCounter/IncrementCounterCommand.cs
+++ src/Application/Counters/Commands/IncrementCounter/IncrementCounterCommand.cs
@@ -1,5 +1,19 @@
-﻿using MediatR;
+﻿using FluentValidation;
+
+using MediatR;
 
 namespace TeamCounters.Application.Counters.Commands.IncrementCounter;
 
 public sealed record IncrementCounterCommand(Guid CounterId, int Value) : IRequest<int>;
+
+public sealed class IncrementCounterCommandValidator : AbstractValidator<IncrementCounterCommand>
+{
+    public IncrementCounterCommandValidator()
+    {
+        RuleFor(x => x.CounterId)
+            .NotEmpty();
+
+        RuleFor(x => x.Value)
+            .GreaterThan(0);
+    }
+}
